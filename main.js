eb9c82a65a8e9d8329574bfe7f09e4ba11ead716
gus0660 <guillaumeroch3@gmail.com>
2024-02-22 12:57:21 +0100

    mon footer ne fonctionne toujours pas

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function includeNavbar() {
 
   // Définissez le contenu de la barre de navigation
   navbar.innerHTML = `
-    <div class="container">
+  <div class="container">
     <button
       class="navbar-toggler"
       id="menu-toggler"
@@ -17,8 +17,7 @@ function includeNavbar() {
       data-bs-target="#navbarNav"
       aria-controls="navbarNav"
       aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
+      aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
@@ -27,23 +26,23 @@ function includeNavbar() {
           <img src="images/logoKESKIADM.webp" class="navbar-logo d-none d-md-block" alt="Logo" width="201" height="90" style="margin-right: auto; margin-left: auto; margin-right: 30px;">
         </a>
         <li class="nav-item">
-      <div class="nav-content">
-        <a class="nav-link" href="destination.html">DESTINATION</a>
-        <span class="nav-subtext">Carte - Trajet</span>
-      </div>
-    </li>
-    <li class="nav-item">
-      <div class="nav-content">
-        <a class="nav-link" href="decouvrir.html">DECOUVRIR</a>
-        <span class="nav-subtext">Explorez</span>
-      </div>
-    </li>
-    <li class="nav-item">
-      <div class="nav-content">
-        <a class="nav-link" href="organisation.html">ORGANISATION</a>
-        <span class="nav-subtext">De votre séjour</span>
-      </div>
-    </li>
+          <div class="nav-content">
+            <a class="nav-link" href="destination.html">DESTINATION</a>
+            <span class="nav-subtext">Carte - Trajet</span>
+          </div>
+        </li>
+        <li class="nav-item">
+          <div class="nav-content">
+            <a class="nav-link" href="decouvrir.html">DECOUVRIR</a>
+            <span class="nav-subtext">Explorez</span>
+          </div>
+        </li>
+        <li class="nav-item">
+          <div class="nav-content">
+            <a class="nav-link" href="organisation.html">ORGANISATION</a>
+            <span class="nav-subtext">De votre séjour</span>
+          </div>
+        </li>
     
       </ul>
       <button class="btn btn-secondary d-block d-sm-none" id="back-button">
@@ -59,15 +58,72 @@ function includeNavbar() {
   // Ajoutez la barre de navigation à cet élément
   navbarContainer.appendChild(navbar);
 }
-document.addEventListener("DOMContentLoaded", function () {
-  var menuToggler = document.querySelector("#menu-toggler");
-  var backButton = document.querySelector("#back-button");
 
-  menuToggler.addEventListener("click", function () {
-    if (menuToggler.getAttribute("aria-expanded") === "true") {
-      backButton.style.display = "block";
-    } else {
-      backButton.style.display = "none";
-    }
-  });
-});
+// function includeFooter() {
+//   // Créez un nouvel élément <footer> pour contenir le footer
+//   var footer = document.createElement("footer");
+//   footer.classList.add("footer");
+
+//   // Définissez le contenu du footer
+//   footer.innerHTML = `
+//   <div class="container">
+//   <button
+//     class="navbar-toggler"
+//     id="menu-toggler"
+//     type="button"
+//     data-bs-toggle="collapse"
+//     data-bs-target="#navbarNav"
+//     aria-controls="navbarNav"
+//     aria-expanded="false"
+//     aria-label="Toggle navigation"
+//   >
+//     <span class="navbar-toggler-icon"></span>
+//   </button>
+//   <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+//     <ul class="footer-menu">
+//       <li class="nav-item">
+//         <div class="nav-content">
+//           <a class="nav-link" href="destination.html">DESTINATION</a>
+//           <span class="nav-subtext">Carte - Trajet</span>
+//         </div>
+//       </li>
+//       <li class="nav-item">
+//         <div class="nav-content">
+//           <a class="nav-link" href="decouvrir.html">DECOUVRIR</a>
+//           <span class="nav-subtext">Explorez</span>
+//         </div>
+//       </li>
+//       <li class="nav-item">
+//         <div class="nav-content">
+//           <a class="nav-link" href="organisation.html">ORGANISATION</a>
+//           <span class="nav-subtext">De votre séjour</span>
+//         </div>
+//       </li>
+  
+//     </ul>
+//     <button class="btn btn-secondary d-block d-sm-none" id="back-button">
+//       <a href="index.html" style="text-decoration: none; color: white;">Retour</a>
+//     </button>
+//   </div>
+// </div>
+//   `;
+
+//   // Trouvez l'élément sur la page où vous souhaitez inclure le footer
+//   var footerContainer = document.querySelector("#footer-container");
+
+//   // Ajoutez le footer à cet élément
+//   footerContainer.appendChild(footer);
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var menuToggler = document.querySelector("#menu-toggler");
+//   var backButton = document.querySelector("#back-button");
+
+//   menuToggler.addEventListener("click", function () {
+//     if (menuToggler.getAttribute("aria-expanded") === "true") {
+//       backButton.style.display = "block";
+//     } else {
+//       backButton.style.display = "none";
+//     }
+//   });
+// });
