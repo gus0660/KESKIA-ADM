8e95c6228d2d94f3380c9b32c3dd1acf9bfef41a
gus0660 <guillaumeroch3@gmail.com>
2024-02-26 14:48:02 +0100

    ok fonction showAdresse

--- destination.js
+++ destination.js
@@ -21,6 +21,67 @@ const map = new ol.Map({
   }),
 });
 
+async function showAddress() {
+  var address = document.querySelector('textarea').value; // Récupère l'adresse du textarea
+  var coords = await getCoordinatesFromAddress(address); // Convertit l'adresse en coordonnées
+
+  if (coords) {
+      var olCoords = ol.proj.fromLonLat(coords);
+      map.getView().animate({center: olCoords, zoom: 16});
+
+      var iconStyle = new ol.style.Style({
+          image: new ol.style.Icon({
+              src: 'images/icon_marqueur.png',
+              scale: 0.5
+          })
+      });
+
+      var iconFeature = new ol.Feature({
+          geometry: new ol.geom.Point(olCoords)
+      });
+
+      iconFeature.setStyle(iconStyle);
+
+      var layer = new ol.layer.Vector({
+          source: new ol.source.Vector({
+              features: [iconFeature]
+          })
+      });
+      
+      map.addLayer(layer);
+  } else {
+      alert("Adresse non trouvée.");
+  }
+}
+async function getCoordinatesFromAddress(address) {
+  var apiKey = "5b3ce3597851110001cf6248265456eaefdf40ca9d7ce5ce7a189570";
+
+  try {
+      const response = await fetch(
+          `https://api.openrouteservice.org/geocode/search?api_key=${apiKey}&text=${encodeURIComponent(address)}`
+      );
+      const data = await response.json();
+
+      if (data.features && data.features.length > 0) {
+          // Prend la première fonctionnalité de la réponse
+          const firstFeature = data.features[0];
+          const coords = firstFeature.geometry.coordinates;
+          // Les coordonnées sont retournées sous forme [longitude, latitude]
+          return [coords[0], coords[1]];
+      } else {
+          // Gérer le cas où aucune fonctionnalité n'est trouvée
+          console.log("Aucune adresse trouvée");
+          return null;
+      }
+  } catch (error) {
+      console.error("Erreur lors du géocodage: ", error);
+      return null;
+  }
+}
+
+
+
+
 function geocodeAddress(address) {
   var apiKey = "5b3ce3597851110001cf6248265456eaefdf40ca9d7ce5ce7a189570";
   fetch(
