1182ad429956cb337b508fd64853bd14f18cba2b
gus0660 <guillaumeroch3@gmail.com>
2024-02-26 14:49:17 +0100

    suppression commentaires et autres inutiles de destination.js

--- destination.js
+++ destination.js
@@ -80,22 +80,6 @@ async function getCoordinatesFromAddress(address) {
 }
 
 
-
-
-function geocodeAddress(address) {
-  var apiKey = "5b3ce3597851110001cf6248265456eaefdf40ca9d7ce5ce7a189570";
-  fetch(
-    `https://api.openrouteservice.org/geocode/search?api_key=${apiKey}&text=${encodeURIComponent(
-      address
-    )}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      // Traitez les données de géocodage ici
-    })
-    .catch((error) => console.log(error));
-}
-
 function calculateRoute(startCoords, endCoords) {
   var apiKey = "5b3ce3597851110001cf6248265456eaefdf40ca9d7ce5ce7a189570";
   var requestBody = {
@@ -149,45 +133,3 @@ function showMyLocation() {
     });
   }
 }
-
-// document.getElementById('bouton_geoloc').addEventListener('click', function() {
-//   if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(function(position) {
-//           var lon = position.coords.longitude;
-//           var lat = position.coords.latitude;
-
-//           // Créer une icône
-//           var iconFeature = new ol.Feature({
-//               geometry: new ol.geom.Point(ol.proj.fromLonLat([lon, lat]))
-//           });
-
-//           var iconStyle = new ol.style.Style({
-//               image: new ol.style.Icon({
-//                   src: 'images/icon_marqueur.png'
-//               })
-//           });
-
-//           iconFeature.setStyle(iconStyle);
-
-//           var vectorSource = new ol.source.Vector({
-//               features: [iconFeature]
-//           });
-
-//           var vectorLayer = new ol.layer.Vector({
-//               source: vectorSource
-//           });
-
-//           // Ajouter l'icône à la carte
-//           map.addLayer(vectorLayer);
-
-//           // Centrer la carte sur la position
-//           map.getView().setCenter(ol.proj.fromLonLat([lon, lat]));
-//           map.getView().setZoom(12); // Ajustez le niveau de zoom si nécessaire
-//       }, function(error) {
-//           // Gérer les erreurs ici
-//           console.log('Erreur de géolocalisation: ' + error.message);
-//       });
-//   } else {
-//       alert("La géolocalisation n'est pas prise en charge par ce navigateur.");
-//   }
-// });
