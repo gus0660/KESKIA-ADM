1db8d9cf54a87822cf0ce90d0d102ed11ccf825a
gus0660 <guillaumeroch3@gmail.com>
2024-02-27 16:56:45 +0100

    ok j'y suis presque

--- destination.js
+++ destination.js
@@ -76,11 +76,21 @@ async function showLocation(address) {
   let formattedCoords = formatCoordsForAPI(coords);
   displayOnMap(coords);
   return formattedCoords;
-}
 
-function formatCoordsForAPI(coords) {
-  return coords.map(c => c.toString().trim()).join(',');
+}function formatCoordsForAPI(coords) {
+  // D'abord, supprimer les espaces potentiels et convertir en chaîne
+  const formattedCoords = coords.map(c => c.toString().trim());
+  // Ensuite, convertir chaque élément de la chaîne en nombre
+  return formattedCoords.map(c => parseFloat(c));
 }
+  
+// function formatCoordsForAPI(coords) {
+//   // Convertit chaque coordonnée en chaîne de caractères et supprime les espaces
+//   return coords.map(c => c.toString().trim());
+// }
+// function formatCoordsForAPI(coords) {
+//   return coords.map(c => c.toString().trim()).join(',');
+// }
 
 function displayOnMap(coords) {
   var olCoords = ol.proj.fromLonLat(coords);
@@ -107,36 +117,52 @@ function displayOnMap(coords) {
   map.addLayer(layer);
 }
 
-function showMyLocation() {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var coords = [position.coords.longitude, position.coords.latitude];
-      var olCoords = ol.proj.fromLonLat(coords);
-      map.getView().animate({ center: olCoords, zoom: 14 });
+async function showMyLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
 
-      var iconStyle = new ol.style.Style({
-        image: new ol.style.Icon({
-          src: "images/icon_marqueur.png",
-          scale: 0.5
-        }),
+          // Format the coordinates for the API
+          const formattedCoords = formatCoordsForAPI([longitude, latitude]);
+
+          // Convertir les coordonnées pour OpenLayers
+          var olCoords = ol.proj.fromLonLat([longitude, latitude]);
+
+          // Style de l'icône
+          var iconStyle = new ol.style.Style({
+              image: new ol.style.Icon({
+                  src: "images/icon_marqueur.png",
+                  scale: 0.5
+              }),
+          });
+
+          // Créer la fonctionnalité avec l'icône
+          var iconFeature = new ol.Feature({
+              geometry: new ol.geom.Point(olCoords),
+          });
+          iconFeature.setStyle(iconStyle);
+
+          // Créer et ajouter la couche vectorielle
+          var layer = new ol.layer.Vector({
+              source: new ol.source.Vector({
+                  features: [iconFeature],
+              }),
+          });
+
+          map.addLayer(layer);
+
+          // Centre la vue sur la position de l'utilisateur
+          map.getView().animate({center: olCoords, zoom: 10});
+
+      }, (error) => {
+          console.error("Erreur lors de l'obtention de la position : ", error);
       });
-      var iconFeature = new ol.Feature({
-        geometry: new ol.geom.Point(olCoords),
-      });
-
-      iconFeature.setStyle(iconStyle);
-
-      // Créer et ajouter la couche vectorielle
-      var layer = new ol.layer.Vector({
-        source: new ol.source.Vector({
-          features: [iconFeature],
-        }),
-      });
-
-      map.addLayer(layer);
-    });
+  } else {
+      console.error("La géolocalisation n'est pas prise en charge par ce navigateur.");
   }
 }
+
 // Variables globales pour stocker les coordonnées
 let startPoint = null;
 let endPoint = null;
@@ -195,35 +221,99 @@ function handleRouteCalculation() {
 }
 
 
+// async function calculateAndDisplayRoute(startPoint, endPoint) {
+//   console.log("startPoint:", startPoint, "endPoint:", endPoint);
+//   console.log("Coordonnées de départ pour le calcul de l'itinéraire:", startPoint);
+//   console.log("Coordonnées d'arrivée pour le calcul de l'itinéraire:", endPoint);
+//   if (!startPoint || !endPoint) {
+//       alert("Veuillez spécifier à la fois un point de départ et un point d'arrivée.");
+//       return;
+//   }
+
+//   const apiKey = "5b3ce3597851110001cf6248265456eaefdf40ca9d7ce5ce7a189570";
+//   const requestBody = {
+//     coordinates: [startPoint, endPoint],
+//     profile: "driving-car",
+//     format: "json",
+//   };
+
+//   try {
+//       const response = await fetch("https://api.openrouteservice.org/v2/directions/driving-car", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: apiKey,
+//         },
+//         body: JSON.stringify(requestBody),
+//       });
+//       const data = await response.json();
+
+//       console.log(data)// me dit : {error: {…}, info: {…}}
+//       console.log(data.error)
+//       if (data.features && data.features.length > 0) {
+//           const route = data.features[0];
+//           const routeGeometry = new ol.format.GeoJSON().readGeometry(route.geometry);
+
+//           const routeFeature = new ol.Feature({
+//               type: 'route',
+//               geometry: routeGeometry
+//           });
+
+//           const routeLayer = new ol.layer.Vector({
+//               source: new ol.source.Vector({
+//                   features: [routeFeature]
+//               }),
+//               style: new ol.style.Style({
+//                   stroke: new ol.style.Stroke({
+//                       width: 6,
+//                       color: [40, 40, 200, 0.8]
+//                   })
+//               })
+//           });
+
+//           map.addLayer(routeLayer);
+//           map.getView().fit(routeGeometry, { padding: [100, 100, 100, 100] });
+//       } else {
+//           console.log("Aucun itinéraire trouvé");
+//       }
+//   } catch (error) {
+//       console.error("Erreur de calcul de l'itinéraire:", error);
+//   }
+// }
+
 async function calculateAndDisplayRoute(startPoint, endPoint) {
+  // Log pour vérifier si startPoint et endPoint sont des tableaux
+  console.log("Is startPoint an array?", Array.isArray(startPoint));
+  console.log("Is endPoint an array?", Array.isArray(endPoint));
+
+  if (!Array.isArray(startPoint) || !Array.isArray(endPoint) || startPoint.length !== 2 || endPoint.length !== 2) {
+      alert("Veuillez spécifier à la fois un point de départ et un point d'arrivée.");
+      return;
+  }
   console.log("startPoint:", startPoint, "endPoint:", endPoint);
-  console.log("Coordonnées de départ pour le calcul de l'itinéraire:", startPoint);
-  console.log("Coordonnées d'arrivée pour le calcul de l'itinéraire:", endPoint);
-  if (!startPoint || !endPoint) {
+  if (!startPoint || !endPoint || startPoint.length !== 2 || endPoint.length !== 2) {
       alert("Veuillez spécifier à la fois un point de départ et un point d'arrivée.");
       return;
   }
 
   const apiKey = "5b3ce3597851110001cf6248265456eaefdf40ca9d7ce5ce7a189570";
   const requestBody = {
-    coordinates: [startPoint, endPoint],
-    profile: "driving-car",
-    format: "json",
+      coordinates: [startPoint, endPoint],
+      profile: "driving-car",
+      format: "json",
   };
 
   try {
       const response = await fetch("https://api.openrouteservice.org/v2/directions/driving-car", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: apiKey,
-        },
-        body: JSON.stringify(requestBody),
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+              Authorization: apiKey,
+          },
+          body: JSON.stringify(requestBody),
       });
       const data = await response.json();
 
-      console.log(data)
-      
       if (data.features && data.features.length > 0) {
           const route = data.features[0];
           const routeGeometry = new ol.format.GeoJSON().readGeometry(route.geometry);
